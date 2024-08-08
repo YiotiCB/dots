bcd582226ebfb1724943c06c7c586c203955601b
Yioti <panknst@gmail.com>
2024-08-08 14:19:48 +0100

    I don't remember what I changed last but pushing anw

--- nvim/init.lua
+++ nvim/init.lua
@@ -66,6 +66,8 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+--Remap jj to Esc:
+vim.keymap.set('i', 'jk', '<Esc>', {})
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -49,10 +49,25 @@ return {
       --
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
+
+        --Self explanatory below
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+          if cmp.visible() then
+            local entry = cmp.get_selected_entry()
+            if not entry then
+              cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+            end
+            cmp.confirm()
+          else
+            fallback()
+          end
+        end, { 'i', 's', 'c' }),
+
         -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-j>'] = cmp.mapping.select_next_item(),
         -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-k>'] = cmp.mapping.select_prev_item(),
 
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
